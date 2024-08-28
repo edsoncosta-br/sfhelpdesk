c037b07699c98f3dfb0c776f1596712c3d5f5f59
Edson Costa <edson@sofolha.com.br>
2024-08-28 16:08:12 -0300

    development

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -74,6 +74,16 @@ class ProjectsController < ApplicationController
                     where('project_id = ?', params[:request][:project_id]);
     else
       @marks = ''
+    end
+
+    if !params[:request][:project_id].empty?
+      @users_responsible = Allocation.select('users.id', 'users.nick_name')
+                                      .joins(:user)
+                                      .joins(user: :company)
+                                      .where("company_id = ? and allocations.project_id = ?", current_user.company.id, params[:request][:project_id])
+                                      .order("unaccent(users.nick_name)")
+    else
+      @users_responsible = ''
     end    
   end    
 
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,12 +3,27 @@ class RequestsController < ApplicationController
   before_action :set_upcase, only: %i[ create update ]    
 
   def index
-    requests = Request.all
-    #  where("company_id = ?", current_user.company.id)
-    #                   .order(Arel.sql('unaccent(description)'))
+    requests = Request.select(:id, :title, :status, :step, :priority, :customer_id,
+                              :user_created_id, :user_responsible_id, :mark_id, :topic_id,
+                              :sub_topic_id, "topics.description topic_description", "sub_topics.description subtopic_description")
+                      .joins(project: :company)
+                      .joins(:topic)
+                      .joins(:sub_topic)
+                      .where("company_id = ?", current_user.company.id)
+                      .order(Arel.sql('status'))
 
-    if !params[:q_desc].blank?
-      requests = requests.where('unaccent(description) ilike unaccent(?)', "%#{params[:q_desc]}%")
+    if params[:q_sys].blank?
+      params[:q_sys] = Methods.select_allocations(current_user.company.id, current_user.id)
+
+      if params[:q_sys].blank?
+        params[:q_sys] = 0
+      end                                  
+    end
+
+    requests = requests.where('projects.id = ?', "#{params[:q_sys]}")    
+
+    if !params[:q_content].blank?
+      requests = requests.where('unaccent(title) ilike unaccent(?)', "%#{params[:q_content]}%")
     end    
 
     @requests = requests.all.page(params[:page]).per(Constants::PAGINAS)
@@ -20,11 +35,7 @@ class RequestsController < ApplicationController
     @request.status = Constants::STEP_ABERTA[1]
     @request.step = Constants::STATUS_AGUARDANDO[1]
     @request.created_date = DateTime.now()
-
-    @request.project_id = Allocation.joins(project: :company)
-                                    .where("company_id = ? and allocations.user_id = ?", 
-                                            current_user.company.id, current_user.id)
-                                    .pick('projects.id')
+    @request.project_id = params[:q_sys]
   end
 
   def create
@@ -33,10 +44,45 @@ class RequestsController < ApplicationController
     
     respond_to do |format|
       if @request.save
-        format.html { redirect_to requests_path(q_desc: params[:q_desc]), notice: "Requisição cadastrada com sucesso." }
+        format.html { redirect_to requests_path(q_sys: params[:q_sys],
+                                                q_content: params[:q_content]), notice: "Requisição cadastrada com sucesso." }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @request.update(request_params)
+      redirect_to requests_path(q_sys: params[:q_sys],
+                                q_content: params[:q_content]), notice: "Requisição atualizada com sucesso."
+    else
+      render :edit
+    end      
+  end
+
+  def destroy
+    begin
+      if @request.destroy
+        redirect_to requests_path(q_sys: params[:q_sys],
+                                  q_content: params[:q_content]), notice: "Tópico excluído com sucesso."
+      else
+        redirect_to requests_path(q_sys: params[:q_sys],
+                                  q_content: params[:q_content])
+      end
+    rescue StandardError => e
+
+      if e.message.downcase.to_s.include? "foreignkeyviolation"
+        flash[:error] = "Este registro já está sendo usado e não pode ser excluído!"
+      else  
+        flash[:error] = e.message[0...80] + "..."
+      end
+
+      redirect_to requests_path(q_sys: params[:q_sys],
+                                q_content: params[:q_content])
     end
   end  
 
