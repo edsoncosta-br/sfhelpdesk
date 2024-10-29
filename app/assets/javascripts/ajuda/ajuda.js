15c5cbbb30007840648e1e15b372e72c1db83efa
Edson Costa <edson@sofolha.com.br>
2024-10-29 16:09:35 -0300

    development

--- app/assets/javascripts/ajuda/ajuda.js
+++ app/assets/javascripts/ajuda/ajuda.js
@@ -1,3 +1,15 @@
 //= require trix
 //= require actiontext
 //= require ./../fontawesome/all.js
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const trixContent = document.querySelectorAll('.trix-content')
+  trixContent.forEach(function(trix) {
+    trix.querySelectorAll('a').forEach(function(link) {
+      if (link.host !== window.location.host) {
+        link.target = "_blank"
+      }
+    })      
+  })    
+});
