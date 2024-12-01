4043875f0a35add0e12b16e897b2f0bf3f655a69
Edson <edsonbcosta@gmail.com>
2024-12-01 19:59:38 -0300

    dev

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -1,4 +1,10 @@
 class PanelsController < ApplicationController
   def index
+    @projects = Allocation.select('projects.id', :description)
+                          .joins(:project)
+                          .joins(project: :company)
+                          .where("company_id = ? and allocations.user_id = ?", current_user.company.id, current_user.id)
+                          .order("main desc, unaccent(description)")
   end
 end
+
